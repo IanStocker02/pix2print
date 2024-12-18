31ef71b00fe03ec2cead4b27fd2eb672819a64f9
joey vedder <qilotog1@gmail.com>
2024-12-17 20:18:03 -0500

    Made login functionality work and styled the login box

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,21 +3,50 @@ import SignupModal from '../components/SignupModal';
 
 const LoginPage = () => {
   const [isSignupOpen, setSignupOpen] = useState(false);
+  const [username, setUsername] = useState(''); 
+  const [password, setPassword] = useState(''); 
+  const [isLoggedIn, setIsLoggedIn] = useState(false); 
 
   const openSignupModal = () => setSignupOpen(true);
   const closeSignupModal = () => setSignupOpen(false);
 
+  const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (username && password) {
+      setIsLoggedIn(true);
+    }
+  };
+
   return (
     <div>
       <h1>Login</h1>
-      <form>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button>Login</button>
+
+      {isLoggedIn ? (
         <div>
-          <a href="#" onClick={openSignupModal}>Don't have an account? Sign up now!</a>
+          <h2>Hello, {username}!</h2>
+          <p>Welcome back to Pix2Print.</p>
         </div>
-      </form>
+      ) : (
+        <form onSubmit={handleLogin}>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Login</button>
+          <div>
+            <a href="#" onClick={openSignupModal}>Don't have an account? Sign up now!</a>
+          </div>
+        </form>
+      )}
+
       <SignupModal isOpen={isSignupOpen} onClose={closeSignupModal} />
     </div>
   );
